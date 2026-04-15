d994b22dfe932f6efa8af36e2d5f51ff500e91d7
Sayan Karmakar <134129431+KarmakarSayan@users.noreply.github.com>
2026-04-15 03:43:58 -0400

    latest code

--- azure_logs.py
+++ azure_logs.py
@@ -47,6 +47,7 @@ dependencies
 | where name == "POST /ACHCheckPrescreen/GetReport"
 | where success != true
 | project name, appId, target, success, resultCode, ["TimeStamp(UTC)"] = timestamp
+| order by timestamp desc
 """
 
 query_2 = f"""
